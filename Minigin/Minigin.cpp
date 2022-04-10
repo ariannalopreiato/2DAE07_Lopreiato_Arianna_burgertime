209f1309fab97b60364b8cb8112ab7d7aad3854a
ariannalopreiato <ariannalopreiato@hotmail.com>
2022-04-10 15:16:00 +0200

    Project setup
    Contains:
       * Minigin
       * A different solution for the game

---
+++ Minigin/Minigin.cpp
@@ -0,0 +1,93 @@
+#include "MiniginPCH.h"
+#include "Minigin.h"
+#include <thread>
+#include "InputManager.h"
+#include "SceneManager.h"
+#include "Renderer.h"
+#include "ResourceManager.h"
+#include "TextObject.h"
+#include "GameObject.h"
+#include "Scene.h"
+
+using namespace std;
+
+void PrintSDLVersion()
+{
+	SDL_version compiled{};
+	SDL_version linked{};
+
+	SDL_VERSION(&compiled);
+	SDL_GetVersion(&linked);
+	printf("We compiled against SDL version %d.%d.%d ...\n",
+		compiled.major, compiled.minor, compiled.patch);
+	printf("We are linking against SDL version %d.%d.%d.\n",
+		linked.major, linked.minor, linked.patch);
+}
+
+void dae::Minigin::Initialize()
+{
+	PrintSDLVersion();
+	
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+	{
+		throw std::runtime_error(std::string("SDL_Init Error: ") + SDL_GetError());
+	}
+
+	m_Window = SDL_CreateWindow(
+		"Programming 4 assignment",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+	if (m_Window == nullptr) 
+	{
+		throw std::runtime_error(std::string("SDL_CreateWindow Error: ") + SDL_GetError());
+	}
+
+	Renderer::GetInstance().Init(m_Window);
+}
+
+/**
+ * Code constructing the scene world starts here
+ */
+void dae::Minigin::LoadGame() const
+{
+	//auto& scene = SceneManager::GetInstance().CreateScene("Demo");
+}
+
+void dae::Minigin::Cleanup()
+{
+	Renderer::GetInstance().Destroy();
+	SDL_DestroyWindow(m_Window);
+	m_Window = nullptr;
+	SDL_Quit();
+}
+
+void dae::Minigin::Run()
+{
+	Initialize();
+
+	// tell the resource manager where he can find the game data
+	ResourceManager::GetInstance().Init("../Data/");
+
+	LoadGame();
+
+	{
+		auto& renderer = Renderer::GetInstance();
+		auto& sceneManager = SceneManager::GetInstance();
+		auto& input = InputManager::GetInstance();
+
+		// todo: this update loop could use some work.
+		bool doContinue = true;
+		while (doContinue)
+		{
+			doContinue = input.ProcessInput();
+			sceneManager.Update();
+			renderer.Render();
+		}
+	}
+
+	Cleanup();
+}
